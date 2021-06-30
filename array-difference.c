c910111b21238a639a3352d8dccc2ad1535905b3
spacekay <86255369+spacekay@users.noreply.github.com>
2021-07-01 00:31:42 +0900

    deleted unnecessary codes

--- array-difference.c
+++ array-difference.c
@@ -5,18 +5,16 @@ int main(void)
     int arr[10];
     int brr[10];
     int t=0;
-    int a;
 
     for (int i=0 ; i < 10 ; i++)
     {
         scanf("%d", &arr[i]);
 
-        a = (arr[i]%42);
         brr[i]=0;
 
         for (int j = 0 ; j < i+1 ; j++)
         {
-            if (a != (arr[j]%42))
+            if ((arr[i]%42) != (arr[j]%42))
             {
                 brr[i]++;
             }
