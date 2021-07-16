13dcb981b15765c05bb56efd73a0638da7b0c18c
spacekay <86255369+spacekay@users.noreply.github.com>
2021-07-16 19:06:54 +0900

    corrected wrong answer

--- Hwno3_6.java
+++ Hwno3_6.java
@@ -6,10 +6,8 @@ public class Hwno3_6 {
 		int lengthBottom = 10;
 		int Height = 7;
 		
-		double area = (double)( (lengthTop+lengthBottom) * Height/2);
+		double area = (double)( (lengthTop+lengthBottom) * Height/2.0);
 		System.out.println(area);
-		
-		// 52.0
+		// 52.5	
 	}
-
 }
