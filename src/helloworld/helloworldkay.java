6253e92a725844b61114aefff9dba15984d2048a
spacekay <86255369+spacekay@users.noreply.github.com>
2021-07-19 18:59:33 +0900

    Delete src/helloworld directory

--- src/helloworld/helloworldkay.java
+++
@@ -1,9 +0,0 @@
-package helloworld;
-
-public class helloworldkay {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("SPACE KAY");
-		System.out.println("Keep hustling");
-	}
-	}
