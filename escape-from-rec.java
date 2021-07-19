fa575b22a1d3b86a5abab0bc3c448ee75a461762
spacekay <86255369+spacekay@users.noreply.github.com>
2021-07-19 15:56:34 +0900

    baekjoon #1085

---
+++ escape-from-rec.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x, y, w, h;
+		try (Scanner sc = new Scanner(System.in)) {
+		x = sc.nextInt();
+		y = sc.nextInt();
+		w = sc.nextInt();
+		h = sc.nextInt();
+		}
+		
+		if ((w-x) >= x && (h-y) >= y) {
+			if (x > y)
+				System.out.println(y);
+			else
+				System.out.println(x);
+		}
+		else if ((w-x) < x && (h-y) >= y) {
+			if ((w-x) > y)
+				System.out.println(y);
+			else
+				System.out.println(w-x);
+		}	
+		else if ((w-x) < x && (h-y) < y) {
+			if ((w-x) > (h-y))
+				System.out.println(h-y);
+			else
+				System.out.println(w-x);
+		}
+		else {
+			if (x > (h-y))
+				System.out.println(h-y);
+			else
+				System.out.println(x);
+		}
+	}
+}
