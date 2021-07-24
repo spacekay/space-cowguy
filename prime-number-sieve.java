67d0eb01c50cc5fdc28d13dee385f6e7cee30117
spacekay <86255369+spacekay@users.noreply.github.com>
2021-07-25 00:40:12 +0900

    baekjoon #1929

---
+++ prime-number-sieve.java
@@ -0,0 +1,34 @@
+package primenumber;
+
+import java.util.Scanner;
+
+public class N1929 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		// 단순히 true/false를 구분하기 위한 array라면 boolean을 쓰는 게 빨라진다.
+		boolean[] prime = new boolean[n+1];
+
+		prime[0] = true;
+		prime[1] = true;
+
+		for (int j = 2 ; j <= n ; j++) {
+			for (int k = 2 ; k*j <= n ; k++) { 
+				// k*j <= n 이런 방식으로도 for문 쓸 수 있음에 대한 발견
+				prime[j*k] = true;
+			}
+		}
+		
+		// for문이나 if문은 최대한 중첩하지 말자.
+		for (int i = m ; i <= n ; i++) {
+			if (prime[i] == false)
+				System.out.println(i);
+		}	
+		sc.close();
+	}
+}
