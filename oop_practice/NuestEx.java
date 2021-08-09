5d704aea6d3c7e7453531fb0b167937704c8a1c5
spacekay <86255369+spacekay@users.noreply.github.com>
2021-08-09 10:10:48 +0900

    initial
    oop introduction (21. 7. 27)

---
+++ oop_practice/NuestEx.java
@@ -0,0 +1,19 @@
+package oop_practice;
+
+public class NuestEx {
+
+	public static void main(String[] args) {
+
+		Nuest nuest = new Nuest();
+		
+		System.out.println("<전체인사>");
+		nuest.allGreet();
+		
+		nuest.cmg = new StableMember("렌", "막내", 27, "뉴이스트");
+		
+		System.out.println("<별도인사>");
+		nuest.allGreet();
+
+	}
+
+}
