78b57596103dfdd1191c3ec73aa77f0915ef885d
spacekay <86255369+spacekay@users.noreply.github.com>
2021-08-09 10:57:12 +0900

    Rename Hwno2.java to javaclass/Hwno2.java

--- javaclass/Hwno2.java
+++ javaclass/Hwno2.java
@@ -2,7 +2,7 @@ package hw;
 
 public class Hwno2 {
 	public static void main(String[] args) {
-		String name = "±èÁ¾Èñ";
+		String name = "Â±Ã¨ÃÂ¾ÃˆÃ±";
 		String tel = "010 7237 7695";
 		System.out.println(name + " " + tel);
 	}
