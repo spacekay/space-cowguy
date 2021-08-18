f5d18e2355e3b5a7dc58ce0f748e45660157ccb2
spacekay <86255369+spacekay@users.noreply.github.com>
2021-08-18 22:34:23 +0900

    baekjoon #1316

--- baekjoon/GroupWordChecker.java
+++ baekjoon/GroupWordChecker.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-public class Main {
+public class GroupWordChecker {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
