e4725041332ee58f7bb5b413c54aeef11910b615
spacekay <86255369+spacekay@users.noreply.github.com>
2021-08-18 22:33:42 +0900

    baekjoon #1316

---
+++ baekjoon/group_word_checker.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int x = Integer.parseInt(br.readLine());
+		int t = 0;
+
+		for (int i = 0 ; i < x ; i++) {
+			String word = br.readLine();
+			char char_present = word.charAt(0);
+			int l = word.length();
+			boolean flag = false;
+			List<String> list = new ArrayList<String>();
+			for (int j = 0 ; j < l ; j++) {
+				if (char_present == word.charAt(j)) {
+					list.add(Character.toString(word.charAt(j)));
+				} else if (list.contains(Character.toString(word.charAt(j)))) {
+					flag = true;
+					break;
+				} else {
+					list.add(Character.toString(word.charAt(j)));
+					char_present = word.charAt(j);
+				}
+			}
+			if (!flag)
+				t++;
+		}
+		sb.append(t);
+		System.out.println(sb);
+		br.close();
+	}
+}
