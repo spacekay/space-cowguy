aac65fc45435194c426dd1a1af8a0e517357df7e
spacekay <86255369+spacekay@users.noreply.github.com>
2021-08-23 01:02:00 +0900

    third (#11866)
    deleted unnecessary code and corrected the class name

--- baekjoon/JosephusProblem.java
+++ baekjoon/JosephusProblem.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class JosephusProblem {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		StringBuilder sb = new StringBuilder();
