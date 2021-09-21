8e6d09fc636d048109ffdf3a131393216041e265
spacekay <86255369+spacekay@users.noreply.github.com>
2021-09-21 17:08:08 +0900

    baekjoon #1463
    revision 1 : save array 사용을 멈추고, int 변수 3개로 바꿔줌

--- baekjoon/MakeOne.java
+++ baekjoon/MakeOne.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 
 public class MakeOne {
 	static int[] arr;
+	static int big=100001;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,8 +20,11 @@ public class MakeOne {
 		bw.close();
 	}
 	public static int countMin(int n) {
-		// 아래 save를 하나의 변수 값으로 단순화해도 메모리 점유가 낮을 수 있음
-		int[] save= {100001,100001,100001};
+		// 아래 save를 하나의 변수 값으로 단순화하면 메모리 점유도 낮아지고 속도도 빨라짐
+		// 객체 하나 만드는 거 자체가 오래걸림
+		int save0=0;
+		int save1=0;
+		int save2=0;
 		if(n==1) {
 			return 0;
 		}
@@ -28,15 +32,20 @@ public class MakeOne {
 			return arr[n];
 		}
 		if(n%3==0) {
-			save[0]=countMin(n/3)+1;
-		} 
+			save0=countMin(n/3)+1;
+		} else {
+			save0=big;
+		}
 		if(n%2==0) {
-			save[1]=countMin(n/2)+1;
-		} 
-		save[2]=countMin(n-1)+1;
+			save1=countMin(n/2)+1;
+		} else {
+			save1=big;
+		}
+		save2=countMin(n-1)+1;
+		//System.out.println(save0+" "+save1+" "+save2);
 		// 아무리 작은 배열이라도 sort보다는 Math.min/max가 더 빠름 (두 수만 비교하는 거니까)
-		int result=Math.min(save[0], save[1]);
+		int result=Math.min(save0, save1);
 		// 현재의 수에서 세가지 케이스 돌려 나온 값이 적은 것을 이 자연수의 최솟값으로 저장
-		return arr[n]=Math.min(save[2], result);
+		return arr[n]=Math.min(save2, result);
 	}
 }
