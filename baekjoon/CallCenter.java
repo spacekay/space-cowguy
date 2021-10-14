efdc06e69ad6df814af97b5e09f73c4d24e80308
spacekay <86255369+spacekay@users.noreply.github.com>
2021-10-14 23:50:56 +0900

    baekjoon #5339

---
+++ baekjoon/CallCenter.java
@@ -0,0 +1,15 @@
+public class CallCenter {
+  // 가끔은 쉬어가자
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
