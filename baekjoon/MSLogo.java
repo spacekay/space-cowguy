3cea06f6c66c00b1fc8cec2229b46d8bef01de3f
spacekay <86255369+spacekay@users.noreply.github.com>
2021-10-18 20:14:39 +0900

    baekjoon #5338

---
+++ baekjoon/MSLogo.java
@@ -0,0 +1,10 @@
+// Import 하나 없다니.. 가끔은 chill time도 필요하다
+public class MSLogo {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
