64d4a6e4d5f53810b249f8d46661e014eff139e1
spacekay <86255369+spacekay@users.noreply.github.com>
2021-10-24 20:39:42 +0900

    baekjoon #11729
    재귀를 너무 어렵게 생각하지 말자.

--- baekjoon/HanoiTower.java
+++ baekjoon/HanoiTower.java
@@ -1,17 +1,10 @@
-package recursive0;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
 
-//  미완성
 public class HanoiTower {
-	static Stack<Integer> s1 = new Stack<>();
-	static Stack<Integer> s2 = new Stack<>();
-	static Stack<Integer> s3 = new Stack<>();
 	static int cnt=0;
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
@@ -19,109 +12,23 @@ public class HanoiTower {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
 		br.close();
-		for(int i=n;i>0;i--) {
-			s1.add(i);
-		}
-		//System.out.println(s1.isEmpty());
-		moveDisk(1);
+		moveDisk(n,1,2,3);
 		bw.write(cnt+"\n");
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
-	public static void moveDisk(int i) {
-		if(s1.isEmpty()&&s2.isEmpty())
+	public static void moveDisk(int n, int start, int mid, int to) {
+		cnt++;
+		if(n==1) {
+			sb.append(start).append(" ").append(to).append("\n");
 			return;
-		if(i==1) {
-			if(s1.isEmpty()) {
-				moveDisk(2);
-			} else if(s3.isEmpty()) {
-				System.out.println("1 3 "+s1.peek());
-				sb.append("1 3\n");
-				s3.push(s1.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s3.peek()>s1.peek()) {
-				System.out.println("1 3 "+s1.peek());
-				sb.append("1 3\n");
-				s3.push(s1.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s2.isEmpty()) {
-				System.out.println("1 2 "+s1.peek());
-				sb.append("1 2\n");
-				s2.push(s1.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s2.peek()>s1.peek()) {
-				System.out.println("1 2 "+s1.peek());
-				sb.append("1 2\n");
-				s2.push(s1.pop());
-				cnt++;
-				moveDisk(1);
-			} else {
-				moveDisk(2);
-			}
-		} else if(i==2) {
-			if(s2.isEmpty()) {
-				moveDisk(3);
-			} else if(s3.isEmpty()) {
-				System.out.println("2 3 "+s2.peek());
-				sb.append("2 3\n");
-				s3.push(s2.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s3.peek()>s2.peek()) {
-				System.out.println("2 3 "+s2.peek());
-				sb.append("2 3\n");
-				s3.push(s2.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s1.isEmpty()) {
-				System.out.println("2 1 "+s2.peek());
-				sb.append("2 1\n");
-				s1.push(s2.pop());
-				cnt++;
-				moveDisk(2);
-			} else if(s2.peek()>s1.peek()) {
-				System.out.println("2 1 "+s2.peek());
-				sb.append("2 1\n");
-				s1.push(s2.pop());
-				cnt++;
-				moveDisk(2);
-			} else {
-				moveDisk(3);
-			}
-		} else {
-			if(s3.isEmpty()) {
-				moveDisk(1);
-			} else if(s2.isEmpty()) {
-				System.out.println("3 2 "+s3.peek());
-				sb.append("3 2\n");
-				s2.push(s3.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s2.peek()>s3.peek()) {
-				System.out.println("3 2 "+s3.peek());
-				sb.append("3 2\n");
-				s2.push(s3.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s1.isEmpty()) {
-				System.out.println("3 1 "+s3.peek());
-				sb.append("3 1\n");
-				s1.push(s3.pop());
-				cnt++;
-				moveDisk(1);
-			} else if(s1.peek()>s3.peek()) {
-				System.out.println("3 1 "+s3.peek());
-				sb.append("3 1\n");
-				s1.push(s3.pop());
-				cnt++;
-				moveDisk(1);
-			} else {
-				moveDisk(1);
-			}
 		}
-	}
+		// 맨 끝에 n개를 얹으려면 n-1개를 중앙에 얹어놔야 함
+		moveDisk(n-1,start,to,mid);
+		// 그러고 가장 큰 원반을 오른쪽으로 옮김
+		sb.append(start).append(" ").append(to).append("\n");
+		// 그다음 중앙에 쌓인 n-1개를 다시 그 위에 옮김
+		moveDisk(n-1,mid,start,to);
+	} 
 }
