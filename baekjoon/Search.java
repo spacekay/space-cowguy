78f72a0fe934b1d8d7c571d55e050e5130171afa
spacekay <86255369+spacekay@users.noreply.github.com>
2021-10-26 22:37:10 +0900

    baekjoon #1786

--- baekjoon/Search.java
+++ baekjoon/Search.java
@@ -1,12 +1,10 @@
-package string0;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-// 어렵다. 미완성임
+// 역시 어려운게 맞았음.. 첫 플래티넘 자축!
 public class Search {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,41 +25,32 @@ public class Search {
 				table[i]=index;
 			}
 		}
+		StringBuilder sb =new StringBuilder();
 		index=0;
 		int cnt=0;
-		int start=0;
-		int tmpstart=0;
-		boolean flag=false;
-		int t=0;
 		for(int i=0;i<L;i++) {			
-			if(index>0&& P.charAt(index)!=S.charAt(i)) {
+			while(index>0&&P.charAt(index)!=S.charAt(i)) {
 				index = table[index-1];
-				flag=false;
-				//System.out.println(i+" "+S.charAt(i));
-			}
+			}			
 			if(P.charAt(index)==S.charAt(i)) {
-				if(!flag) {
-					tmpstart=t;
-					//System.out.println(i+" "+S.charAt(i));
-					index++;
-					flag=true;
-				} else if(index==l-1) {
-					start=tmpstart;
-					index=0;
-					flag=false;
+				if(index==l-1) {
+					// 해당 패턴의 시작 인덱스는 무조건 여기임
+					// 끝날때만 해당 패턴의 시작 인덱스를 보여주면 됨 (깔-끔)
+					sb.append(i-l+2).append(" ");
+					// 바로 다음 문자 대상으로 탐색 가능한지 확인
+					index = table[index];
+					// table[l-1]은 사실상 접두사의 길이, index는 접두사 직후 문자열이 됨
+					// 접두사-접미사가 같다는걸 가정했으므로 같은 패턴이 연이어 나와도 접두사+접미사 길이만큼은 탐색 안해도 됨
 					cnt++;
 				} else {
 					index++;
 				}
 			}
-			if(S.charAt(i)!=' ')
-				t++;
 		}
 		bw.write(cnt+"\n");
 		//if()
-		bw.write((start+1)+"\n");
+		bw.write(sb.toString()+"\n");
 		bw.flush();
 		bw.close();
 	}
-
 }
