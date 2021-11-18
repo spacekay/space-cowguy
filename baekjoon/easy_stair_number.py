e95ee51afee1797468468e054c0187e2a42a7701
spacekay <86255369+spacekay@users.noreply.github.com>
2021-11-18 19:26:40 +0900

    baekjoon #10844

--- baekjoon/easy_stair_number.py
+++ baekjoon/easy_stair_number.py
@@ -1,19 +1,23 @@
 # baekjoon #10844
 # Easy Stair Number
 
-# 미완성!
 from sys import stdin
 
 n = int(stdin.readline())
 
-arr = [[0 for _ in range(10)] for _ in range(n)]
-for i in range(1, 11):
+arr = [[0 for _ in range(10)] for _ in range(n+2)]
+for i in range(1, 10):
     arr[1][i] = 1
 
 # 어떤 수를 만들기 위해서는 대체로 이전에 두가지 수를 사용할 수 있음
 
 for i in range(2, n+1):
-    for j in range(0, 11):
-        
+    for j in range(0, 10):
+        if j == 0:
+            arr[i][j] = arr[i-1][j+1]
+        elif j == 9:
+            arr[i][j] = arr[i-1][j-1]
+        else:
+            arr[i][j] = arr[i-1][j-1]+arr[i-1][j+1]
 
-print(arr[-1])
+print(sum(arr[n]) % 1000000000)
