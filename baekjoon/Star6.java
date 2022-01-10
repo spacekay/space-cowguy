c06c6357b239c4ef7d9f8c05e2f1182c6be80208
spacekay <86255369+spacekay@users.noreply.github.com>
2022-01-10 19:32:19 +0900

    baekjoon #2443

---
+++ baekjoon/Star6.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Star6 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		for(int i=n;i>=1;i--) {
+			for(int j=0;j<=2*(n-1);j++) {
+				if(j>n-1-i&&j<n-1+i)
+					bw.write("*");
+				else if (j<n-1)
+					bw.write(" ");
+				else
+					break;
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
