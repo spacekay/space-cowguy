6507a6b17a24ee1b1df22bff831f6a334beeda48
spacekay <86255369+spacekay@users.noreply.github.com>
2022-01-13 00:07:40 +0900

    baekjoon #17298

--- baekjoon/BigRightNumber.java
+++ baekjoon/BigRightNumber.java
@@ -26,11 +26,16 @@ public class BigRightNumber {
 			// 스택에서 뽑은 인덱스 값이 현재 인덱스 값보다 작으면 현재 값으로 대체
 			// (스택에서 뽑은 인덱스 값에서 가장 가까운 값이므로)
 			while(!stack.isEmpty() && ans[stack.peek()]<ans[i]) {
+				// While문 돌아갈 때마다 점점 앞쪽 인덱스로 가게 됨
 				ans[stack.pop()] = ans[i];
 			}
+			// 현재 인덱스 값이 스텍에서 뽑은 인덱스 값보다 작으므로 그 위에 쌓임
 			stack.push(i); // 배열의 인덱스
 		}
 		
+		// Good Test Case : 5 \n 5 1 3 2 4
+		
+		// 끝까지 쌓여있는 인덱스 : 자기 오른쪽에 자기보다 큰 값이 없는 인덱스 (상대적으로 값이 큰 인덱스)
 		while(!stack.isEmpty())
 			ans[stack.pop()] = -1;
 		
