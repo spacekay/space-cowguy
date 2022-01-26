0f17c5955aba4d764379da28d5660ab6f0acaddd
spacekay <86255369+spacekay@users.noreply.github.com>
2022-01-26 19:39:20 +0900

    baekjoon #10926

---
+++ baekjoon/QQE.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QQE {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String id = br.readLine();
+		br.close();
+		System.out.println(id+"??!");
+	}
+}
