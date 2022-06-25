9877daac100a5d4440b5bced4f3997c5ca4e7011
spacekay <86255369+spacekay@users.noreply.github.com>
2022-06-25 17:21:38 +0900

    baekjoon #11651
    solved the time issue using the custom Comparator object

--- baekjoon/SortPoints.java
+++ baekjoon/SortPoints.java
@@ -9,41 +9,31 @@ import java.util.TreeMap;
 public class SortPoints {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;		
-		int x;
-		int y;
-		// 큰 수의 정렬을 위해서는 무작정 이차원 배열 만들고 시작하면 안된다.
-		ArrayList<Integer> list;
-		TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
-		for (int i = 0; i<n;i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			if (!map.containsKey(y)) {
-        // 없을 때는 새로 만들어주기만 하고 바로 넣기
-				list = new ArrayList<Integer>();
-				list.add(x);
-			} else {
-        // 이미 원소 있을 때만 리스트에 성분 넣어주고 정렬 미리 해주기
-				list = map.get(y);
-				list.add(x);
-				Collections.sort(list);
-			}
-			map.put(y, list);
+		int[][] list = new int[n][2];
+		StringTokenizer st;
+		for(int i = 0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			list[i][0] = Integer.parseInt(st.nextToken());
+			list[i][1] = Integer.parseInt(st.nextToken());
 		}
-		StringBuilder sb = new StringBuilder();
-		while (!map.isEmpty()) {
-			y = map.firstKey();
-			list = map.get(y);
-			for (int i = 0; i<list.size(); i++) {
-				x=list.get(i);
-				sb.append(x).append(" ");
-				sb.append(y).append("\n");
-			}
-			map.remove(y);
-		}
-		System.out.println(sb.toString());
 		br.close();
+
+		Arrays.sort(list, new Comparator<int[]>() {
+		@Override
+		public int compare(int[] o1, int[] o2) {
+				if(o1[1]==o2[1]) {
+					return o1[0]-o2[0];
+				}		
+				return o1[1]-o2[1];
+			}
+		});
+
+		for(int i=0;i<n;i++) {
+			bw.write(list[i][0]+" "+list[i][1]+"\n");
+		}	
+		bw.flush();
+		bw.close();
 	}
 }
