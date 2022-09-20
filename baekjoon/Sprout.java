959e8e7a1db82ab66b135bb15562bbd738f96ddb
spacekay <86255369+spacekay@users.noreply.github.com>
2022-09-20 23:47:11 +0900

    baekjoon #25083

---
+++ baekjoon/Sprout.java
@@ -0,0 +1,13 @@
+public class Sprout
+{
+    // tip: arguments are passed via the field below this editor
+    public static void main(String[] args)
+    {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
