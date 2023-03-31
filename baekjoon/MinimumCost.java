bee4a6a19225c855c5262dc8d84c02453452ee8b
spacekay <86255369+spacekay@users.noreply.github.com>
2023-03-31 23:09:58 +0900

    baekjoon #1916
    second (boundary case, array 초기화 빈틈 보충 및 반복문 기입 오류 수정)

--- baekjoon/MinimumCost.java
+++ baekjoon/MinimumCost.java
@@ -6,7 +6,7 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class MinimunCost {
+public class MinimumCost {
 	static int[][] bus;
 	static int n;
 	static int m;
@@ -44,17 +44,30 @@ public class MinimunCost {
 		bw.flush();
 		bw.close();
 	}
-  // Dijkstra algorithm 사용법 익힘! (가중치가 음수가 아닐 때에 탐색하는 방법->최소값을 갱신)
+  // Dijkstra algorithm 사용법 익힘! (가중치가 음수가 아닐 때에 탐색하는 방법 -> 최소값을 갱신)
 	public static void minCost(int a, int b) {		
 		visited = new boolean[n+1];
 		cost = new long[n+1];
+		Arrays.fill(cost, inf);
+		
+		// 출발지와 목적지가 같은 경우에는 버스를 타지 않아도 됨
+		if (a == b) {
+			cost[b] = 0;
+			return;
+		}
+		
 		for(int i=1;i<=n;i++) {
 			cost[i]=bus[a][i];
 		}
 		visited[a]=true;
-		for(int i=0;i<b-2;i++) {
+		
+		// 전체 도시에 대해서 네트워크를 탐색해야 하므로 안전하게는 n회 탐색이 필요하다.
+		// 반복문 작성 오류가 '틀렸습니다' 원인인 것으로 추정
+		for(int i=0;i<n-1;i++) {
 			int now = minIndex();
 			visited[now]=true;
+			
+			// 버스 번호와 array index를 동일하게 맞춰 놓았기 때문에 반복문 범위를 1 to n으로 설정
 			for(int j=1;j<=n;j++) {
 				if(!visited[j]) { // 최소값으로 갱신해주기
 					if(cost[now]+bus[now][j]<cost[j]) {
@@ -66,7 +79,7 @@ public class MinimunCost {
 	}
 	public static int minIndex() {
 		int index=0;
-		long min= inf;
+		long min= inf + 1;
 		for(int i=1;i<=n;i++) {
 			if(cost[i]<min && !visited[i]) {
 				min = cost[i];
