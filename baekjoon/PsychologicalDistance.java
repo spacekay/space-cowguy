58bb44c57ea88af3742123513626de4335b67049
spacekay <86255369+spacekay@users.noreply.github.com>
2023-09-11 22:57:52 +0900

    baekjoon #20529

--- baekjoon/PsychologicalDistance.java
+++ baekjoon/PsychologicalDistance.java
@@ -3,84 +3,114 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
+
+// 20529
 
 public class PsychologicalDistance {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    Map<String, Integer> mbtiMap = new HashMap<>();
-    Set<String> checked = new HashSet<>();
-    Queue<Integer> save = new PriorityQueue<>();
-    StringTokenizer st;
-    int n;
+        Map<String, Integer> mbtiMap = new HashMap<>();
+        StringTokenizer st;
+        int n;
 
-    int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-    outer:
-    for (int k = 0; k < T; k++) {
-      n = Integer.parseInt(br.readLine());
-      mbtiMap.clear();
-      bw.write(n + "====\n");
-      st = new StringTokenizer(br.readLine());
-      if (n > 18) {
-        bw.write("0\n");
-        continue;
-      }
-      for (int i = 0; i < n; i++) {
-        String type = st.nextToken();
-        int now = 0;
-        if (mbtiMap.containsKey(type)) {
-          now = mbtiMap.get(type);
-        }
-        now++;
-        if (now > 2) {
-          bw.write("0\n");
-          continue outer;
-        }
-        mbtiMap.put(type, now);
-      }
+        outer:
+        for (int l = 0; l < T; l++) {
+            n = Integer.parseInt(br.readLine());
+            mbtiMap.clear();
+            st = new StringTokenizer(br.readLine());
 
-      Set<String> keySet = mbtiMap.keySet();
-      bw.write(keySet.size() + "*\n");
-      for (String key : keySet) {
-        checked.add(key);
-        for (String key2 : keySet) {
-          if (!checked.contains(key2)) {
-            save.add(checkDifference(key, key2));
-          }
+          // 비둘기집의 원리 적용
+            if (n > 32) {
+                bw.write("0\n");
+                continue;
+            }
+
+            for (int i = 0; i < n; i++) {
+                String type = st.nextToken();
+                int now = 0;
+                if (mbtiMap.containsKey(type)) {
+                    now = mbtiMap.get(type);
+                }
+                now++;
+              // 같은 MBTI가 3명 이상이면 무조건 결과값은 0
+                if (now > 2) {
+                    bw.write("0\n");
+                    continue outer;
+                }
+                mbtiMap.put(type, now);
+            }
+
+            List<String> keyList = new ArrayList<>(mbtiMap.keySet());
+            // 개수가 많은 MBTI부터 먼저 탐색하도록 함
+            keyList.sort((v1, v2) -> (mbtiMap.get(v2).compareTo(mbtiMap.get(v1))));
+
+            int size = keyList.size();
+            int min = Integer.MAX_VALUE;
+
+            for (int i = 0; i < size; i++) {
+              // 똑같은 MBTI가 2명인 경우 둘 간의 거리는 0이 됨
+                if (mbtiMap.get(keyList.get(i)) == 2) {
+                    for (int j = i + 1; j < size; j++) {
+                        min = Math.min(min, checkDifference(keyList.get(i), keyList.get(i), keyList.get(j)));
+                        if (min == 0) {
+                            bw.write("0\n");
+                            break outer;
+                        }
+                    }
+                } else {
+                    for (int j = i + 1; j < size; j++) {
+                     // 똑같은 MBTI가 2명인 경우 둘 간의 거리는 0이 됨
+                        if (mbtiMap.get(keyList.get(j)) == 2) {
+                            for (int k = j + 1; k < size; k++) {
+                                min = Math.min(min, checkDifference(keyList.get(i), keyList.get(j), keyList.get(j)));
+                                if (min == 0) {
+                                    bw.write("0\n");
+                                    break outer;
+                                }
+                            }
+                        } else {
+                            for (int k = j + 1; k < size; k++) {
+                                min = Math.min(min, checkDifference(keyList.get(i), keyList.get(j), keyList.get(k)));
+                                if (min == 0) {
+                                    bw.write("0\n");
+                                    break outer;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            bw.write(min + "\n");
         }
-        if (mbtiMap.get(key) > 1) {
-          save.add(0);
-        }
-      }
-      int count = 0;
-      for (int j = 0; j < 3 && !save.isEmpty(); j++) {
-        count += save.poll();
-      }
-      bw.write(count + "\n");
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
-    br.close();
-    bw.flush();
-    bw.close();
-  }
-
-  private static int checkDifference(String type1, String type2) {
-    int count = 0;
-    for (int i = 0; i < 4; i++) {
-      if (type1.charAt(i) != type2.charAt(i)) {
-        count++;
-      }
+  // 3가지 MBTI의 거리를 한 번에 측정할 수 있는 메소드
+    private static int checkDifference(String type1, String type2, String type3) {
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            if (type1.charAt(i) != type2.charAt(i)) {
+                count += 1;
+            }
+            if (type1.charAt(i) != type3.charAt(i)) {
+                count += 1;
+            }
+            if (type2.charAt(i) != type3.charAt(i)) {
+                count += 1;
+            }
+        }
+        return count;
     }
-    return count;
-  }
 }
