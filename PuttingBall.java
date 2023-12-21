8e4f2c89876bdda4570e12a0ea2f3d36027d4c71
spacekay <86255369+spacekay@users.noreply.github.com>
2023-12-21 23:45:36 +0900

    baekjoon #10810

---
+++ PuttingBall.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 10810
+
+public class PuttingBall {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] bucket = new int[n];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            for (int j = a; j <= b; j++) {
+                bucket[j - 1] = c;
+            }
+        }
+        br.close();
+        for (int ball : bucket) {
+            bw.write(ball + " ");
+        }
+        bw.write("\n");
+        bw.flush();
+        bw.close();
+    }
+}
