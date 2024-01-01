29f5b2daa634868b0b13a3fba53f11b247d52452
spacekay <86255369+spacekay@users.noreply.github.com>
2024-01-02 00:10:35 +0900

    baekjoon #1250 (파일명 상세하게)

--- baekjoon/PalindromeNumber.java
+++ baekjoon/PalindromeNumber.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Palindrome {
+public class PalindromeNumber {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
